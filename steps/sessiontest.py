2903b7ad95116a3c7798b6bfeed042b7fcb78af1
Mike Sullivan <txsulli60@gmail.com>
2023-08-25 09:27:01 -0500

    snowpark exercise

---
+++ steps/sessiontest.py
@@ -0,0 +1,10 @@
+# import time
+# from snowflake.snowpark import Session
+
+# import sys
+# sys.path.append('/Users/mike/miniconda3/envs/pysnowpark/lib/python3.8/site-packages')
+
+# print(sys.path)
+
+
+print('VSCode Jello')
